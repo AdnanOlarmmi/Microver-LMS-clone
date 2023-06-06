aea416787cf6fa168ad4e23201e9b38291baad16
adnanolarmmi <olarmmi@gmail.com>
2023-06-06 16:15:50 +0100

    Add workers

---
+++ app/workers/course_fetcher_worker.rb
@@ -0,0 +1,20 @@
+class CourseFetcherWorker
+    include Sidekiq::Worker
+  
+    def perform
+      # Fetch courses from the remote API
+      courses = fetch_courses
+  
+      # Update the local database with new courses
+      courses.each do |course|
+        Course.create(name: course['name'], description: course['description'])
+      end
+    end
+  
+    private
+  
+    def fetch_courses
+      # Implement the logic to fetch courses from the remote API
+    end
+  end
+  
