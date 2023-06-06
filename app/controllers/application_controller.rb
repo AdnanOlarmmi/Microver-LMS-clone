e793a95f1aea39517b00162fb01c5908311fdec1
adnanolarmmi <olarmmi@gmail.com>
2023-06-07 00:42:34 +0100

    Add jwt auth to app_ctrlr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
-class ApplicationController < ActionController::Base
+require 'json_web_token'
+
+class ApplicationController < ActionController::API
+  include ExceptionHandler
+
+  before_action :authorize_request
+
+  private
+
+  def authorize_request
+    header = request.headers['Authorization']
+    raise ExceptionHandler::MissingToken unless header
+
+    token = header.split(' ').last
+    decoded_token = JsonWebToken.decode(token)
+    @current_user = User.find(decoded_token[:user_id])
+  rescue ActiveRecord::RecordNotFound, JWT::DecodeError => e
+    raise ExceptionHandler::AuthenticationError, e.message
+  end
 end
