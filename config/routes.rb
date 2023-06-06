7cb160dbee5747a781db3547f055326c4aa35cbe
adnanolarmmi <olarmmi@gmail.com>
2023-06-07 00:52:53 +0100

    Add app routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :users, only: [:create]
-  resources :sessions, only: [:create]
-  resources :courses, only: [:index, :create] do
-    member do
-      post 'enroll'
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create] do
+        get '/enrolled_courses', to: 'users#enrolled_courses', on: :collection
+      end
+      post '/login', to: 'authentication#login'
+      resources :courses, only: [:index] do
+        post '/enroll', to: 'courses#enroll', on: :member
+      end
     end
   end
 end
-
