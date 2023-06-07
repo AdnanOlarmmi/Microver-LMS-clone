68c8737c669237d7aac66c4d8b6d44c6de946586
adnanolarmmi <olarmmi@gmail.com>
2023-06-07 06:25:31 +0100

    Add test for user validation

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+# spec/models/user_spec.rb
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it 'is valid with valid attributes' do
+    user = build(:user)
+    expect(user).to be_valid
+  end
+
+  # Add more tests for other model behaviors
+end
