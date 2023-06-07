6c61ca8e5830a01e99fac7472df25ae49f8f046b
adnanolarmmi <olarmmi@gmail.com>
2023-06-07 07:23:31 +0100

    Add courses model test

---
+++ spec/models/courses_spec.rb
@@ -0,0 +1,20 @@
+# spec/requests/courses_spec.rb
+require 'rails_helper'
+
+RSpec.describe Course, type: :model do
+    describe 'associations' do
+         it 'has many enrollments' do
+            course = Course.reflect_on_association(:enrollments)
+            expect(course.macro).to eq(:has_many)
+         end
+    end
+
+
+    describe 'validations' do
+        it 'is valid with valid attributes' do
+            course = Course.new(name: 'Course 1', description: 'Lorem ipsum')
+            expect(course).to be_valid
+        end
+    end
+  # Add more tests for other endpoints
+end
